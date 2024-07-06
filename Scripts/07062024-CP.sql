86713c1055b952068cfe619508aebe1d0630bfdf
Carlos Piedra <barce90@hotmail.com>
2024-07-06 13:03:08 -0700

    Script Tabla Proveedor - inserts

--- Scripts/07062024-CP.sql
+++ Scripts/07062024-CP.sql
@@ -11,3 +11,10 @@ CREATE TABLE Proveedor (
 
 ALTER TABLE Proveedor
 ADD Constraint PK_Proveedor PRIMARY KEY (ProveedorId);
+
+
+INSERT INTO Proveedor (Nombre, Descripcion, Direccion, Telefono, Email, Estado)
+Values ('Proveedor de Microsoft','Proveedor de Productos y Servicios de Microsoft','352 Broadway ave, New York', '325-989-9896','support@microsoftes.es',1);
+
+INSERT INTO Proveedor (Nombre, Descripcion, Direccion, Telefono, Email, Estado)
+Values ('Proveedor de Mac','Proveedor de Productos y Servicios de Mac','600 Broadway ave, New York', '323-856-9686','support@mac.es',1);
