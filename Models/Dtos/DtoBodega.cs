52b33f1f9aa1b9cf94a4f26ef123578ffc5d5017
Coding Totem <codingtotem@gmail.com>
2024-07-07 16:49:58 -0600

    Agregado Script Tabla Bodega, Creado Modelo Bodega, Agregado DbSet Bodegas, Creado EndPoints para Crear = NewBodega, Actualizar = ModBodega, Eliminar = DelBodega, Consutar = GetBodegas y GetBodega

---
+++ Models/Dtos/DtoBodega.cs
@@ -0,0 +1,15 @@
+namespace Models.Dtos
+{
+
+    public class DtoBodega
+    {
+
+        public string Estado { get; set; }
+
+        public string NombreBodega { get; set; }
+
+        public string Descripcion { get; set; }
+
+    }
+
+}
