b76b4d852d70ab3eba325f0ee2a302ac2299b109
merg8511 <merg8511@gmail.com>
2024-07-07 14:06:08 -0600

    Crear Modelo Proveedor y agregarlo al dbContext

---
+++ Models/Entidades/Proveedor.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Entidades
+{
+    [Table("Proveedor")]
+    public class Proveedor
+    {
+        [Key]
+        [Column("ProveedorId")]
+        public int Id {get; set;}
+
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public bool Estado { get; set; }
+    }
+}
